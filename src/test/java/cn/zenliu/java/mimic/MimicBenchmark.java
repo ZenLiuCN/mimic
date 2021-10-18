2a85257399222fc69bcd7cac7a680e51af5ad686
ZenLiu <ZenLiu.CN@gmail.com>
2021-10-18 18:53:19 +0800

    aa

--- src/test/java/cn/zenliu/java/mimic/MimicBenchmark.java
+++ src/test/java/cn/zenliu/java/mimic/MimicBenchmark.java
@@ -42,23 +42,44 @@ public class MimicBenchmark {
         new Runner(opt.build()).run();
     }
 
+    /**
+     * <pre>
+     *
+     * Benchmark                            Mode  Cnt     Score      Error  Units
+     * MimicBenchmark.proxyMimicBenchBuild  avgt  100  6172.783 ± 2094.918  ns/op
+     * MimicBenchmark.proxyMimicBenchSet    avgt  100  1014.723 ±   96.165  ns/op
+     * </pre>
+     */
     @Test
     void proxyMimic() throws Exception {
-        benchRun("proxyMimicBuild", true);
+        benchRun("proxyMimicBench*", true);
     }
 
     @State(Scope.Thread)
     public static class BenchmarkState {
+        MimicTest.Flue instance;
 
         @Setup(Level.Trial)
         public void initialize() {
-            Mimic.newInstance(MimicTest.Flue.class, null);
+            instance = Mimic.newInstance(MimicTest.Flue.class, null);
+        }
+    }
+
+    @State(Scope.Thread)
+    public static class BenchmarkASMState {
+        MimicTest.Flue instance;
+
+        @Setup(Level.Trial)
+        public void initialize() {
+            instance = Mimic.newInstance(MimicTest.Flue.class, null);
         }
     }
 
     /**
-     * Benchmark                       Mode  Cnt     Score      Error  Units
-     * MimicBenchmark.proxyMimicBuild  avgt  100  4242.168 ± 1732.591  ns/op
+     * <pre>
+     *     Benchmark                       Mode  Cnt     Score      Error  Units
+     *      * MimicBenchmark.proxyMimicBuild  avgt  100  4242.168 ± 1732.591  ns/op
+     * </pre>
      */
     @Benchmark
     @BenchmarkMode(Mode.AverageTime)
@@ -66,7 +87,22 @@ public class MimicBenchmark {
     @Warmup(iterations = warmUpBatchSize, batchSize = warmUpBatchSize, time = warmUpTime, timeUnit = TimeUnit.MICROSECONDS)
     @Timeout(time = timeoutTime)
     @Measurement(iterations = measurementIterations, time = measurementTime, timeUnit = TimeUnit.MICROSECONDS)
-    public void proxyMimicBuild(Blackhole bh) {
-        bh.consume( Mimic.newInstance(MimicTest.Flue.class, null));
+    public void proxyMimicBenchBuild(Blackhole bh) {
+        bh.consume(Mimic.newInstance(MimicTest.Flue.class, null));
+    }
+
+    /**
+     * <pre>
+     *
+     * </pre>
+     */
+    @Benchmark
+    @BenchmarkMode(Mode.AverageTime)
+    @OutputTimeUnit(TimeUnit.NANOSECONDS)
+    @Warmup(iterations = warmUpBatchSize, batchSize = warmUpBatchSize, time = warmUpTime, timeUnit = TimeUnit.MICROSECONDS)
+    @Timeout(time = timeoutTime)
+    @Measurement(iterations = measurementIterations, time = measurementTime, timeUnit = TimeUnit.MICROSECONDS)
+    public void proxyMimicBenchSet(BenchmarkState st, Blackhole bh) {
+        bh.consume(st.instance.id(100L));
     }
 }
